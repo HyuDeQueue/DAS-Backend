6c6a953a1dac8755658a4e82c7ca07ecf78edead
HyuDeQueue <quanghuy01062004@gmail.com>
2024-05-19 13:27:42 +0700

    Adding Backend structure

---
+++ src/main/java/com/project/DASBackend/DasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.DASBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/DASBackend/DasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.DASBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
