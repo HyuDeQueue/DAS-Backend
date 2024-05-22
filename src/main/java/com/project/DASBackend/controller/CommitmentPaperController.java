cb38968eb2210a17d6f1345e8a9a4876c6ffd592
Hyudeque <quanghuy01062004@gmail.com>
2024-05-22 17:32:44 +0700

    Adding base apis for each controllers

--- src/main/java/com/project/DASBackend/controller/CommitmentPaperController.java
+++ src/main/java/com/project/DASBackend/controller/CommitmentPaperController.java
@@ -1,7 +1,9 @@
 package com.project.DASBackend.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/commitmentPapers")
 public class CommitmentPaperController {
 }
