2f1533956ce04d7e572dc1d5d3a3f1152b9c9a22
Hyudeque <quanghuy01062004@gmail.com>
2024-05-25 16:14:10 +0700

    Creating basic CRUD for AssessmentRequest

--- src/main/java/com/project/DASBackend/service/AssessmentRequestService.java
+++ src/main/java/com/project/DASBackend/service/AssessmentRequestService.java
@@ -1,4 +1,17 @@
 package com.project.DASBackend.service;
 
+import com.project.DASBackend.dto.AssessmentRequestDto;
+
+import java.util.List;
+
 public interface AssessmentRequestService {
+    AssessmentRequestDto createAssessmentRequest(AssessmentRequestDto assessmentRequestDto);
+
+    AssessmentRequestDto getAssessmentRequestById(Integer requestId);
+
+    List<AssessmentRequestDto> getAllAssessmentRequests();
+
+    AssessmentRequestDto updateAssessmentRequest(AssessmentRequestDto assessmentRequestDto, Integer requestId);
+
+    void deleteAssessmentRequest(Integer requestId);
 }
