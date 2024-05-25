b4417ab7b1d7cd21b0dbf4bdf9dd02ea8b7b78bd
HyuDeQueue <quanghuy01062004@gmail.com>
2024-05-25 20:51:02 +0700

    Configurations and setups for Account login using OAuth2

---
+++ src/main/java/com/project/DASBackend/controller/AuthController.java
@@ -0,0 +1,18 @@
+package com.project.DASBackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/success")
+    public String success() {
+        return "success";
+    }
+}
